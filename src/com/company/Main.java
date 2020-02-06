9da275a3d2bd601f62ede74c75d29d10ce282077
Zack <ishaqamincoop@icloud.com>
2020-02-06 10:05:11 +0000

    Finished-converter

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int weightinPounds = 550;
+
+        double weightInKilograms = weightinPounds * 0.45359237;
+
+        System.out.println(weightInKilograms);
+    }
+}
