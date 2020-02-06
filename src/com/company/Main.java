3690e32175505ac440f2102a14b92d2db658b192
Zack <ishaqamincoop@icloud.com>
2020-02-06 11:08:53 +0000

    operator-challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int weightinPounds = 550;
 
-        double weightInKilograms = weightinPounds * 0.45359237;
 
-        System.out.println(weightInKilograms);
+        double myFistNumber = 20;
+
+        double mySecondNumber = 80;
+
+        double numbersMultiplied = (myFistNumber + mySecondNumber) * 100.00;
+
+        double myRemainder = numbersMultiplied % 40.00;
+
+        boolean isTrue = myRemainder == 0 ? true:false;
+
+        if (isTrue != true) {
+            System.out.println("Got some remainder!");
+        }
+
+        System.out.println(numbersMultiplied);
+        System.out.println(myRemainder);
+        System.out.println(isTrue);
     }
 }
